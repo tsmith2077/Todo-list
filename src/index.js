0e6db9be96a17660ef5929c03308d50226dd23b9
tsmith2077 <tsmith2077@gmail.com>
2022-04-29 08:22:17 -0600

    Todo list app can add, edit, delete and cancel update request for todo items. Need to add projects and local storage.

--- src/index.js
+++ src/index.js
@@ -1,108 +1,188 @@
 import './style.css';
 
-let todoListArr = [];
+var todoListModule = (function() {
 
-const addItemBtn = document.querySelector('#addItemBtn');
-const allTodoListItems = document.querySelector('.allTodoListItems');
+    let todoListArr = [];
 
-addItemBtn.addEventListener('click', () => {
-    let currentListItem = addListItem();
-    allTodoListItems.appendChild(currentListItem);
-});
+    const addItemBtn = document.querySelector('#addItemBtn');
+    const allListItemsContainer = document.querySelector('.allListItemsContainer');
+    const projectSubmitBtn = document.querySelector('#projectSubmitBtn');
+    const allProjectsContainer = document.querySelector('.allProjectsContainer');
 
-function addListItem() {
-    // Create list item container
-    const listItem = document.createElement('div');
-    listItem.classList.add('listItem');
-    let textInput = document.createElement('input');
-    textInput.classList.add('itemTextInput');
-    textInput.setAttribute("type", "text");
+    // Button event Listeners and functions
+    addItemBtn.addEventListener('click', () => {
+        let currentListItem = addListItem();
+        allListItemsContainer.appendChild(currentListItem);
+    });
 
-    // Create Select input for task priority
-    let prioritySelectInput = createSelect();
-    prioritySelectInput.classList.add('priorityInput');
+    projectSubmitBtn.addEventListener('click', () => {
+        let newProjectName = document.querySelector('#addProjectInput').value;
+        let newProject = createProject(newProjectName);
+        addProjectModal.style.display = "none";
+        allProjectsContainer.appendChild(newProject);
+    });
 
-    // Create submit button
-    let submitBtn = document.createElement('button');
-    submitBtn.classList.add('listItemBtn', 'submitBtn');
-    submitBtn.textContent = "Submit";
-    submitBtn.addEventListener('click', function() { submitListItem(listItem) });
+    // Create new project
+    const createProject = ((newProjectName) => {
+        let newProjectBtn = document.createElement('button');
+        newProjectBtn.textContent = newProjectName;
+        newProjectBtn.classList.add(newProjectName, 'projectNameBtn');
+        let newProjectArr = createNewProjectArr(newProjectName);
+        newProjectBtn.addEventListener('click', function() { showProjectList(newProjectName) });
+        return newProjectBtn, newProjectArr;
+    });
 
-    // Append all list item contents to listItem container
-    listItem.appendChild(textInput);
-    listItem.appendChild(prioritySelectInput);
-    listItem.appendChild(submitBtn);
+    // Creates a new array to hold new project todo list
+    const createNewProjectArr = ((newProjectName) => { 
+        let newProjectTodoList = [];
+        newProjectTodoList.classList.add(newProjectName + 'Arr');
+        return newProjectTodoList;
+    });
 
-    return listItem;
-}
+    // Shows the clicked on project todo list
+    const showProjectList = ((newProjectArr) => {
+        
+    })
 
 
-const createSelect = (() => {
-    const selectArr = ["Low Priority","Medium Priority","High Priority"];
+    // Get the modal
+    var addProjectModal = document.getElementById("addProjectModal");
+    const addProjectBtn = document.getElementById("addProjectBtn");
 
-    //Create and append select list
-    const selectList = document.createElement("select");
-    selectList.id = "selectList";
-    
-    //Create and append the options
-    for (var i = 0; i < selectArr.length; i++) {
-        const option = document.createElement("option");
-        option.value = selectArr[i];
-        option.text = selectArr[i];
-        selectList.appendChild(option);
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    addProjectBtn.addEventListener ('click', () => {
+        addProjectModal.style.display = "block";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        addProjectModal.style.display = "none";
     }
 
-    return selectList;
-})
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == addProjectModal) {
+            addProjectModal.style.display = "none";
+        }
+    }
 
-// Function for creating new todo item
-const createTodoItem = ((title, priority) => {
-    let todoItem = {};
-    todoItem.title = title;
-    todoItem.priority = priority;
-    todoItem.index = (todoListArr.length);
+    const cancelBtnListener = (() => {
+        printTodoListToDom(todoListArr);
+    });
 
-    const addTodoToArr = ((todoItem) => todoListArr.push(todoItem))
+    const editBtnListener = ((event) => {
+        const selectedListItem = event.target.value;
+        printTodoListToDom(todoListArr, selectedListItem);
+    });
 
-    addTodoToArr(todoItem); // add to array todoList
+    const deleteListItem = ((event) => {
+        todoListArr.splice((event.target.parentNode.parentNode.getAttribute('value')), 1);
+        allListItemsContainer.innerHTML = "";
+        printTodoListToDom(todoListArr);
+    });
 
-    printTodoListToDom(todoListArr);
-    return {todoItem, todoListArr};
-})
+    const submitListItem = ((listItem) => {
+        let title = listItem.children[0].value;
+        let priority = listItem.children[1].children[0].value;
+        createTodoItem(title, priority, listItem);
+    });
 
-function submitListItem(listItem) {
-    let title = listItem.children[0].value;
-    let priority = listItem.children[1].value;
+    // Creating a todo list item
+    const createTodoItem = ((title, priority, listItem) => { 
+        let todoItem = {};
+        todoItem.title = title;
+        todoItem.priority = priority;
 
-    createTodoItem(title, priority);
-}
+        if (!listItem.hasAttribute('value')) {
+            const addTodoToArr = ((todoItem) => todoListArr.push(todoItem))
+            addTodoToArr(todoItem); // add to array todoList
+        } else {
+            let currentIndex = listItem.getAttribute('value');
+            todoListArr.splice(currentIndex, 1, todoItem);
+        }
 
-const printTodoListToDom = ((todoListArr) => {
+        printTodoListToDom(todoListArr);
+        return {todoItem, todoListArr};
+    })
 
-    for (var i=0; i<todoListArr.length; i++) {
-        // const todoListItem = createTodoContainer();
-        let todoListItem = document.createElement('div');
-        todoListItem.classList.add('listItem');
-        todoListItem.setAttribute('value', [i]);
+    const printTodoListToDom = ((todoListArr, selectedListItem=undefined) => {
+        allListItemsContainer.innerHTML = "";
+        for (var i=0; i<todoListArr.length; i++) {
+            const todoListItem = createTodoContainer();
+            todoListItem.setAttribute('value', [i]);
+            if (i == selectedListItem || todoListArr == "") {
+                editListItemFormat(todoListItem, todoListArr, i);
+            } else {
+                confirmedListItemFormat(todoListItem, todoListArr, i);
+            }
+        }
+    })
+
+    const editListItemFormat = ((todoListItem, todoListArr, i) => {
+        let textInput = document.createElement('input');
+        textInput.classList.add('itemTextInput');
+        textInput.setAttribute("type", "text");
+
+        // Create Select input for task priority
+        let prioritySelectInput = createSelect();
+        prioritySelectInput.classList.add('priorityInput');
+
+        // Fill in input to what was previous entered
+        if (todoListArr != undefined){
+            textInput.value = todoListArr[i].title;
+            prioritySelectInput.value = todoListArr[i].priority;
+        }
+
+        const priorityBtnContainer = createPriorityBtnContainer();
+
+        // Create submit button
+        let cancelBtn = document.createElement('button');
+        cancelBtn.classList.add('listItemBtn', 'cancelBtn');
+        cancelBtn.textContent = "Cancel";
+        cancelBtn.addEventListener('click', function() { cancelBtnListener(todoListItem) });
+
+        // Create submit button
+        let submitBtn = document.createElement('button');
+        submitBtn.classList.add('listItemBtn', 'submitBtn');
+        submitBtn.textContent = "Submit";
+        submitBtn.addEventListener('click', function() { submitListItem(todoListItem) });
+
+        // Append all list item contents to listItem container
+        todoListItem.appendChild(textInput);
+        priorityBtnContainer.appendChild(prioritySelectInput);
+        priorityBtnContainer.appendChild(cancelBtn);
+        priorityBtnContainer.appendChild(submitBtn);
+        todoListItem.appendChild(priorityBtnContainer);
+        allListItemsContainer.appendChild(todoListItem);
+
+        return todoListItem, i;
+    });
+
+    const confirmedListItemFormat = ((todoListItem, todoListArr, i) => {
+
         const checkbox = createCheckbox();
 
         // Todo item Description
         const todoTextContent = document.createElement('p');
         todoTextContent.classList.add('todoTextContent');
-        todoTextContent.textContent = todoListArr[i].title;
+        todoTextContent.textContent = todoListArr[i].title; 
 
         const priorityBtnContainer = createPriorityBtnContainer();
 
         // Todo item Priority
         const todoPriority = document.createElement('p');
         todoPriority.classList.add('todoPriority');
-        todoPriority.textContent = todoListArr[i].priority;
+        todoPriority.textContent = todoListArr[i].priority; // removed [i]
 
         // Todo Buttons
         const editBtn = createBtn('edit');
         editBtn.setAttribute('value', [i]);
+        editBtn.addEventListener('click', editBtnListener);
         const deleteBtn = createBtn('delete');
-        deleteBtn.setAttribute('value', [i]);
+
         deleteBtn.addEventListener('click', deleteListItem)
 
         todoListItem.appendChild(checkbox);
@@ -111,38 +191,65 @@ const printTodoListToDom = ((todoListArr) => {
         priorityBtnContainer.appendChild(editBtn);
         priorityBtnContainer.appendChild(deleteBtn);
         todoListItem.appendChild(priorityBtnContainer);
-        allTodoListItems.appendChild(todoListItem);
-    }
-})
+        allListItemsContainer.appendChild(todoListItem);
+        return todoListItem;
+    });
 
-const createTodoContainer = (() => {
-    let todoListItem = document.createElement('div');
-    todoListItem.classList.add('listItem');
-    return todoListItem
-})
 
-const createPriorityBtnContainer = (() => {
-    let priorityBtnContainer = document.createElement('div');
-    priorityBtnContainer.classList.add('priorityBtnContainer');
-    return priorityBtnContainer;
-})
+    const addListItem = (() => {
+        // Create list item container
+        const listItem = document.createElement('div');
+        listItem.classList.add('listItem');
+        editListItemFormat(listItem);
 
-const deleteListItem = ((event) => {
-    todoList.splice(event.target.value, 1);
-    allTodoListItems.innerHTML = "";
-    printTodoListToDom(todoListArr);
-});
+        return listItem;
+    })
 
-function createCheckbox() {
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.classList.add('checkbox');
-}
+    const createTodoContainer = (() => {
+        let todoListItem = document.createElement('div');
+        todoListItem.classList.add('listItem');
+        if (todoListArr == ""){
+            editListItemFormat(todoListItem);
+        }
+        return todoListItem;
+    })
 
-const createBtn = ((btnName) => {
-    const btn = document.createElement('button');
-    btn.classList.add('listItemBtn', btnName);
-    btn.textContent = btnName;
+    // Prioirty and button container for todo list items
+    const createPriorityBtnContainer = (() => {
+        let priorityBtnContainer = document.createElement('div');
+        priorityBtnContainer.classList.add('priorityBtnContainer');
+        return priorityBtnContainer;
+    })
 
-    return btn;
-});
+    const createSelect = (() => {
+        const selectArr = ["Low Priority","Medium Priority","High Priority"];
+
+        //Create and append select list
+        const selectList = document.createElement("select");
+        selectList.id = "selectList";
+        //Create and append the options
+        for (var i = 0; i < selectArr.length; i++) {
+            const option = document.createElement("option");
+            option.value = selectArr[i];
+            option.text = selectArr[i];
+            selectList.appendChild(option);
+        }
+        return selectList;
+    })
+
+    const createCheckbox = (() => {
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.classList.add('checkbox');
+        return checkbox;
+    })
+
+    const createBtn = ((btnName) => {
+        const btn = document.createElement('button');
+        btn.classList.add('listItemBtn', btnName);
+        btn.textContent = btnName;
+
+        return btn;
+    });
+
+})();
